bbea62554814f7c15615f82430b7363558aaaf64
Dimitri <dimitri.dg23@gmail.com>
2020-01-29 15:15:37 +0100

    Debut routeur and controleur

--- Controleur/controleur.php
+++ Controleur/controleur.php
@@ -1,3 +1,27 @@
 <?php
     // Controleur
+
+    function ControleurListeLivre(){
+        include 'Vues/booksList.php';
+    }
+
+    function ControleurLogin(){
+        include 'Vues/login.php';
+    }
+
+    function ControleurValidationLogin(){
+        //Appelle de la méthode dans le modèle pour vérifier l'user dans la BD
+        //retour à la page de livres
+        include 'Vues/booksList.php';
+    }
+
+    function ControleurRegister(){
+        include 'Vues/register.php';
+    }
+
+    function ControleurValidationRegister(){
+        //Appelle de la méthode dans le modèle pour remplir l'user dans la BD si valide
+        //retour à la page de livres
+        include 'Vues/booksList.php';
+    }
 ?>
--- index.php
+++ index.php
@@ -1,3 +1,35 @@
 <?php
-    header('Location: Vues/booksList.php');
+
+    require_once 'Modele\modele.php';
+    require_once 'Controleur\controleur.php';
+    if(session_status() == PHP_SESSION_NONE){
+        session_start();
+    }
+    //On vérifiras l'user dans la session dans les vues et affichage en fonction
+
+    //Verification des routes
+    if(isset($_GET['action'])){
+        if($_GET['action'] == 'login'){
+            if($_GET['type'] == 'login'){
+                ControleurLogin();
+            }elseif($_GET['type'] == 'validation'){
+                ControleurValidationLogin();
+            }else{
+                ControleurListeLivre();
+            }
+        }elseif($_GET['action'] == 'register'){
+            if($_GET['type'] == 'register'){
+                ControleurRegister();
+            }elseif ($_GET['type'] == 'validation'){
+                ControleurValidationRegister();
+            }else{
+                ControleurListeLivre();
+            }
+        }
+    }else{
+        ControleurListeLivre();
+    }
+
+
+
 ?>
