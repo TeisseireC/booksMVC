2d445d84ee25bd86d9d2214b392c54392763ab5e
Dimitri <dimitri.dg23@gmail.com>
2020-01-30 09:37:19 +0100

    Login/register Vues

--- Vues/login.php
+++ Vues/login.php
@@ -1,1 +1,23 @@
-<?php
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+
+<form action="index.php?action=login&type=validation" method="post">
+    <label>E-mail
+        <input type="email" name="email">
+    </label>
+    <label>Mot de passe
+        <input type="password" name="password">
+    </label>
+    <button type="submit">Connection</button>
+</form>
+
+</body>
+</html>
